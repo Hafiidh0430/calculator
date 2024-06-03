da753e708349307bc6e4a38f8e1017ade5f64c37
Hafiidh0430 <129365807+Hafiidh0430@users.noreply.github.com>
2024-06-03 18:30:49 +0700

    upload

---
+++ script.js
@@ -0,0 +1,66 @@
+const displayValues = document.querySelector(".display_values");
+const inputValues = document.querySelector(".input_values");
+const inputs = document.querySelectorAll(".inputs");
+let mask = document.querySelector(".mask");
+const operators = ["+", "-", "/", "*", "%"];
+
+inputs.forEach((input) => {
+  input.addEventListener("click", (event) => {
+    inputValues.focus();
+    const regex = /^[0-9+\-*/.() ]+$/;
+    const value = event.target.value;
+
+    if (event.target.id == "equal") {
+      displayValues.style.fontSize = "2rem";
+      inputValues.style.fontSize = "1.4rem";
+    }
+
+    if (event.target.id == "number") {
+      inputValues.style.fontSize = "2rem";
+      displayValues.style.fontSize = "1.4rem";
+    }
+
+    if (value === "deleteAll") {
+      inputValues.value = "";
+      displayValues.textContent = "= ";
+    } else if (value === "delete") {
+      const deleteValue = inputValues.value.substring(
+        0,
+        inputValues.value.length - 1
+      );
+      inputValues.value = deleteValue;
+      displayValues.textContent = `= ${deleteValue}`;
+
+      inputValues.style.fontSize = "2rem";
+      displayValues.style.fontSize = "1.4rem";
+    } else if (value == "( )") {
+      inputValues.focus();
+    } else {
+      //checking if the operator more than 1 before adding new number
+      if (!value && !regex.test(value) && value == undefined) return;
+      if (
+        operators.includes(inputValues.value[inputValues.value.length - 1]) &&
+        operators.includes(value)
+      )
+        return (inputValues.value =
+          inputValues.value.substring(0, inputValues.value.length - 1) + value);
+      if (operators.includes(inputValues.value[inputValues.value.length - 1])) {
+        
+      }
+
+      inputValues.value += value;
+      if (operators.includes(inputValues.value[0])) {
+        displayValues.textContent = "= Input number first before operator!";
+      }
+
+      try {
+        const result = eval(inputValues?.value);
+        displayValues.textContent = `= ${result}`;
+      } catch (error) {}
+    }
+  });
+});
+
+window.addEventListener("load", () => {
+  inputValues.focus();
+});
